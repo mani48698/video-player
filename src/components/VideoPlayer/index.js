5900d420edb220dfa33876f5904ce5766b6080f7
Mani Krishna Chimata <mani48698@gmail.com>
2022-10-02 11:22:36 +0000

    Code

---
+++ src/components/VideoPlayer/index.js
@@ -0,0 +1,14 @@
+import ReactPlayer from 'react-player'
+
+import './index.css'
+
+const VideoPlayer = () => (
+  <div className="video-container">
+    <h1 className="heading">Video Player</h1>
+    <div className="responsive-container">
+      <ReactPlayer url="https://youtu.be/J8lmfiCr7o0" />
+    </div>
+  </div>
+)
+
+export default VideoPlayer
